b370d62d00c975eed020dc537054ddefd94bf999
Dnidof <nidofonsecadiego@gmail.com>
2023-10-14 13:44:34 +0200

    Comentarios en el código: ejemplo de lanzar el comando

--- anfrec.sh
+++ anfrec.sh
@@ -6,4 +6,6 @@ if [ ! -f $1 ]; then
 	exit 1
 fi
 
+# Uso ./anfrec.sh archivo.txt
+
 cat $1 | tr 'a-z' 'A-Z' | awk 'BEGIN{FS=""; for(n=0;n<256;n++){ord[sprintf("%c", n)]=n; frec[n] = 0}; letras=0;} {for(i=1;i<=NF;i++){if($i ~ /[A-Z]/){frec[ord[$i]]++;letras++;}}} END{for(n=0;n<256;n++)if(sprintf("%c", n) ~ /[A-Z]/){ print (frec[n] / letras)*100 " : " sprintf("%c", n)}}' | sort -r -g 
--- md5finder.sh
+++ md5finder.sh
@@ -2,4 +2,6 @@ if [ "$#" -ne 1 ]; then
 	exit 1 
 fi
 
+# Uso ./md5finder.sh HASH_IMAGEN
+
 for i in imagen*; do md5sum $i; done | grep $1 
